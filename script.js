9f3d6aee59b3ddc8f90bcd00dc55aff1769e9a1f
Nozimjon848 <upwork.nozimjon2011@gmail.com>
2024-11-08 08:05:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const A = document.getElementById("chiq");
+function bosish() {
+    if(A.style.display == "none"){
+        A.style.display = "block";
+    }
+    else{
+        A.style.display = "none";
+    }
+    
+}
